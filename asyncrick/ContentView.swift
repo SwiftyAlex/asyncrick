dfc4da103b8611cdd2e2ee24bb747de78605a3ac
Alex Logan <me@alexanderlogan.co.uk>
2021-06-07 23:21:35 +0100

    Initial Commit

---
+++ asyncrick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  asyncrick
+//
+//  Created by Alex Logan on 07/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ asyncrick/asyncrickApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncrickApp.swift
+//  asyncrick
+//
+//  Created by Alex Logan on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct asyncrickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
